508f2de14910d42ce6925086cd3cf01a82cfe305
CSfrogy <lonelyguse@gmail.com>
2026-02-09 21:23:33 +0800

    Photo model function

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,4 +8,19 @@ class Photo extends Model
 {
     /** @use HasFactory<\Database\Factories\PhotoFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'image_path',
+        'original_filename',
+    ];
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function getUrlAttribute(): string
+    {
+        return asset('storage/' . $this->image_path);
+    }
+
 }
